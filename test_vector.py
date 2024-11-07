5590219556dc9ba7d8134cae21677c33cef7df38
TristanBeruard <tristan.beruard@resolvestroke.com>
2024-11-07 23:07:10 +0100

    added vector class

---
+++ test_vector.py
@@ -0,0 +1,10 @@
+from vector import Vector 
+
+
+vecteur1 = Vector(1,1,1)
+vecteur2 = Vector(2,2,2)
+vecteur3 = vecteur2.scaleByLength(2)
+
+print(vecteur1.getXYZ)
+print(vecteur2.getXYZ)
+print(vecteur3.getXYZ)
